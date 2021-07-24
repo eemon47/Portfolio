5b5754c246703c4bc14cbe6f74b112198dfad3cc
eemon47 <emon.cse36@gmail.com>
2021-07-25 02:28:30 +0600

    (Reviws+Work)Updated

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     $('.slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass:'dots'
         
     })
 
